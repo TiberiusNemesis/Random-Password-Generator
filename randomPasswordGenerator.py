f2203322e3ba9952553e26d298bc1e092603054b
TiberiusNemesis <53455509+TiberiusNemesis@users.noreply.github.com>
2021-07-29 15:46:31 -0300

    Add files via upload

---
+++ randomPasswordGenerator.py
@@ -0,0 +1,25 @@
+
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("This is a Python password generator.\n")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+pwd = ""
+rng = 0
+sum = nr_numbers+nr_symbols+nr_letters
+for i in range(sum):
+    rng = random.randint(1,3)
+    if rng == 1:
+        pwd = pwd + random.choice(letters)
+    if rng == 2:
+        pwd = pwd + random.choice(symbols)
+    if rng == 3:
+        pwd = pwd + random.choice(numbers)
+print(f"Your new password is {pwd}")
+
+
